b6d37bcb8eeab3d36fa79c09fcbdcac80f8d31ae
BT_1N3_14 <TYC>
2019-01-03 14:44:30 -0500

    i LIIKE TURTLES

--- src/Swap.java
+++ src/Swap.java
@@ -10,7 +10,7 @@ public class Swap {
         boolean confirm = false;
         while(confirm != true){
             confirm = true;
-            for(int i=0; i <arr.length;i++){
+            for(int i=0; i <arr.length-1;i++){
                 if(arr[i] < arr[i+1]){
                 }
                 if(arr[i] > arr[i+1]){
